c318650b161c56f54f3fa1e2b915939cbf5ff4fb
widyasabina <210323024@fellow.lpkia.ac.id>
2023-08-15 08:12:47 +0700

    contact udh bisa submit

--- src/components/dashboard/contact.dashboard.js
+++ src/components/dashboard/contact.dashboard.js
@@ -1,18 +1,32 @@
-import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import React, { useRef } from "react";
 import { Fade, Slide } from "react-reveal";
+import emailjs from '@emailjs/browser';
+import { Container } from "react-bootstrap";
 
-class Contact extends Component {
-  render() {
+const Contact = () => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_p0jvmjq', 'template_kswwsqm', form.current, 'TKXCdCht69vR_Q5_i')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
     return (
       <>
       <section id="contact">
+        <Container>
       <Slide left duration={1300}>
         <Fade bottom duration={1000}>
           <div className="row section-head">
             <div className="two columns header-col">
               <h1>
-                <span>Get In Touch.</span>
+                <span>Get In Touch</span>
               </h1>
             </div>
 
@@ -23,12 +37,10 @@ class Contact extends Component {
         </Fade>
         </Slide>
 
-        <div className="row">
-          <Slide left duration={1000}>
-            <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
-                <fieldset>
-                  <div>
+                
+                <form ref={form} onSubmit={sendEmail} name="contactForm">
+                    <fieldset>
+                    <div>
                     <label htmlFor="contactName">
                       Name <span className="required">*</span>
                     </label>
@@ -37,12 +49,10 @@ class Contact extends Component {
                       defaultValue=""
                       size="35"
                       id="contactName"
-                      name="contactName"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
+                      name="contactName"/>
+                  
+                    </div>
+                    <div>
                     <label htmlFor="contactEmail">
                       Email <span className="required">*</span>
                     </label>
@@ -51,94 +61,47 @@ class Contact extends Component {
                       defaultValue=""
                       size="35"
                       id="contactEmail"
-                      name="contactEmail"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
+                      name="contactEmail"/>
+                      </div>
+                    <div>
                     <label htmlFor="contactSubject">Subject</label>
                     <input
                       type="text"
                       defaultValue=""
                       size="35"
                       id="contactSubject"
-                      name="contactSubject"
-                      onChange={this.handleChange}
-                    />
-                  </div>
-
-                  <div>
+                      name="contactSubject"/>
+                      </div>
+                      <div>
                     <label htmlFor="contactMessage">
                       Message <span className="required">*</span>
                     </label>
                     <textarea
-                      cols="50"
-                      rows="15"
+                      cols="40"
+                      rows="12"
                       id="contactMessage"
                       name="contactMessage"
                     ></textarea>
-                  </div>
-
-                  <div>
+                    </div>
+                    <div>
                     <button className="submit">Submit</button>
                     <span id="image-loader">
                       <img alt="" src="images/loader.gif" />
                     </span>
                   </div>
-                </fieldset>
+                  </fieldset>
               </form>
-
               <div id="message-warning"> Error boy</div>
               <div id="message-success">
                 <i className="fa fa-check"></i>Your message was sent, thank you!
                 <br />
-              </div>
-            </div>
-          </Slide>
-
-          <Slide right duration={1000}>
-            <aside className="four columns footer-widgets">
-              <div className="widget widget_contact">
-                <h4>Address and Phone</h4>
-              </div>
-
-              <div className="widget widget_tweets">
-                <h4 className="widget-title">Latest Tweets</h4>
-                <ul id="twitter">
-                  <li>
-                    <span>
-                      This is Photoshop's version of Lorem Ipsum. Proin gravida
-                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                      quis bibendum auctor, nisi elit consequat ipsum
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">2 Days Ago</a>
-                    </b>
-                  </li>
-                  <li>
-                    <span>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">3 Days Ago</a>
-                    </b>
-                  </li>
-                </ul>
-              </div>
-            </aside>
-          </Slide>
-        </div>
+              </div>      
+    </Container>
       </section>
       </>
       
     );
   }
-}
+
 
 export default Contact;
